e8a67ff27827954c70d1cd4c303438c8aea936c3
chenzefeng <903266287@qq.com>
2018-01-27 14:36:17 +0800

    王漫增加了登录页面样式

--- GLW_APP-master/js/page/launch/launch.js
+++ GLW_APP-master/js/page/launch/launch.js
@@ -25,7 +25,7 @@ import {
     Text,
     StatusBar,
     TextInput,
-    Button
+    TouchableOpacity
 } from 'react-native';
 
 import { NavigationActions } from 'react-navigation';
@@ -45,26 +45,26 @@ class Launch extends Component {
     }
     onButtonPress(){
         const {navigate} = this.props.navigation;
-            const routeName = 'Tabbar';
-            // const reset = NavigationActions.reset({
-            //     index: 0,
-            //     actions: [NavigationActions.navigate({routeName: 'Tabbar'})]
-            // });
-            // this.props.navigation.dispatch(reset);
+        const routeName = 'Tabbar';
+        // const reset = NavigationActions.reset({
+        //     index: 0,
+        //     actions: [NavigationActions.navigate({routeName: 'Tabbar'})]
+        // });
+        // this.props.navigation.dispatch(reset);
 
-            //登录验证
-            // formData = new FormData();
-            // formData.append("user_name",this.state.user);
-            // formData.append("password",this.state.pw);
-            // console.log(formData);
-            // this.fetchData().then((response)=>{
-            //     console.log(response.code)
-            //     if (response.code===0) {
-            //         navigate('Tabbar');
-            //     }
-            // });
+        //登录验证
+        // formData = new FormData();
+        // formData.append("user_name",this.state.user);
+        // formData.append("password",this.state.pw);
+        // console.log(formData);
+        // this.fetchData().then((response)=>{
+        //     console.log(response.code)
+        //     if (response.code===0) {
+        //         navigate('Tabbar');
+        //     }
+        // });
 
-            navigate('Tabbar');
+        navigate('Tabbar');
     }
 
     async fetchData() {
@@ -87,62 +87,78 @@ class Launch extends Component {
             this.setState({refreshing: false});
         });
     }
-    componentDidMount() {
-        // this.timer = setTimeout(() => {
-        //     //const {navigate} = this.props.navigation;
-        //     const routeName = 'Tabbar';
-        //     const reset = NavigationActions.reset({
-        //         index: 0,
-        //         actions: [NavigationActions.navigate({routeName: 'Tabbar'})]
-        //     });
-        //     this.props.navigation.dispatch(reset);
-        //     // navigate('Tabbar');
-        // }, 2000);
-    }
-
-    componentWillUnmount() {
-        clearTimeout(this.timer);
-    }
-
     render() {
         return (
-            <View style = {styles.view}>
-                <TextInput
-                    style={{height: 40, borderColor: 'gray', borderWidth: 1,width:200}}
-                    onChangeText={(user) => this.setState({user})}
-                    placeholder={"请输入用户名"}
-                    value={this.state.user}
-                />
-                <TextInput
-                    style={{height: 40, borderColor: 'gray', borderWidth: 1,width:200}}
-                    onChangeText={(pw) => this.setState({pw})}
-                    placeholder={"请输入密码"}
-                    secureTextEntry={true}
-                    value={this.state.pw}
-                />
-                <Button
-                    onPress={()=>this.onButtonPress()}
-                    title="点击登录"
-                    color="#841584"
-                    accessibilityLabel="Learn more about purple"
-                />
+            <View style={{width:'100%',height:'100%',backgroundColor:'#f0eff4'}}>
+                <View style={{alignItems:'center',backgroundColor:'#f0eff4'}}>
+                    <Image source={require('../../../res/images/Launch_bg.png')} style={{height: 280,width:'100%'}} />
+                    <Image source={require('../../../res/images/LOGO-Model.png')} style={styles.logImage}/>
+                    <View style={{alignItems:'center',width:'92%',marginTop:-40,backgroundColor:'#fff',borderRadius:5,overflow:'visible'}}>
+                        <Text style={{color:'#5baafb',fontSize:20,marginTop:40,marginBottom:30}}>欢迎登录</Text>
+                        <TextInput
+                            style={styles.TextInput}
+                            onChangeText={(user) => this.setState({user})}
+                            placeholder={"请输入用户名"}
+                            underlineColorAndroid='transparent'
+                            value={this.state.user}
+                        />
+                        <TextInput
+                            style={styles.TextInput}
+                            onChangeText={(pw) => this.setState({pw})}
+                            placeholder={"请输入密码"}
+                            underlineColorAndroid='transparent'
+                            secureTextEntry={true}
+                            value={this.state.pw}
+                        />
+                        <TouchableOpacity style={[styles.center,styles.TouchableOpacity]}>
+                            <Text style={{color:'#fff'}}>点击登录</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    view: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#ffffff',
+    logImage:{
+        height: 80,
+        width:80,
+        borderRadius:50,
+        marginTop:-140,
+        overflow:'visible',
+        zIndex:100,
+        shadowColor:'#f0eff4',
+        shadowRadius:50,
+        shadowOffset:{width: 10, height: 10}
+    },
+    center:{
+        alignItems:'center',
+        justifyContent:'center'
     },
     text: {
         fontSize: 20,
         fontWeight: 'bold',
         color: '#fff',
     },
+    gray:{
+        backgroundColor:'#f0eff4'
+    },
+    TextInput:{
+        height: 40,
+        width:'90%',
+        marginTop:15,
+        borderRadius:20,
+        backgroundColor:'#f0eff4'
+    },
+    TouchableOpacity:{
+        marginTop:30,
+        backgroundColor:'#3396fb',
+        borderRadius:20,
+        width:'90%',
+        height:40,
+        marginBottom:80
+    }
 });
 
 export default Launch;
