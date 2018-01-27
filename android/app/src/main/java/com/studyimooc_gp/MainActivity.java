80a1c88562405f1211658538d70378071dbbf94f
wm <1617672281@qq.com>
2018-01-27 17:03:34 +0800

    更新了

--- android/app/src/main/java/com/studyimooc_gp/MainActivity.java
+++ android/app/src/main/java/com/studyimooc_gp/MainActivity.java
@@ -1,8 +1,35 @@
 package com.studyimooc_gp;
 
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCompat.requestPermissions(this, new String[]{android
+        .Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                }
+            }
+        }
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
